745c607df922846a9f2e31348cc174743f1481e8
PhuongThiLanHuong <125435868+PhuongThiLanHuong@users.noreply.github.com>
2024-03-26 19:17:25 +0700

    service-discovery

---
+++ service-discovery/src/test/java/vn/edu/iuh/fit/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
